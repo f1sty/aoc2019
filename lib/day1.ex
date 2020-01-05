a3f6c613a3f80b88acd1a325c8425241e61c6326
Yurii Skrynnykov <truef1s7@gmail.com>
2020-01-05 22:24:47 +0200

    Add day 1 part 1 solution.

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,1 +1,29 @@
+defmodule Aoc2019.Day1 do
+  @moduledoc false
 
+  @spec required_fuel(mass :: pos_integer) :: integer
+  defp required_fuel(mass), do: div(mass, 3) - 2
+
+  @doc """
+  Calculates total fuel requirement.
+  """
+  @spec total_fuel_requirement(input_stream :: String.t()) :: integer
+  def total_fuel_requirement(input_stream) do
+    input_stream
+    |> Stream.map(fn line ->
+      {mass, _rest} = Integer.parse(line)
+      required_fuel(mass)
+    end)
+    |> Enum.sum()
+  end
+
+  @doc """
+  Runs part 1 solution.
+  """
+  @spec part_one(file_name :: Path.t()) :: integer
+  def part_one(file_name \\ "day1/input.txt") do
+    file_name
+    |> File.stream!()
+    |> total_fuel_requirement()
+  end
+end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -1,4 +1,25 @@
 defmodule Aoc2019.Day1Test do
   use ExUnit.Case, async: true
   doctest Aoc2019.Day1
+
+  setup do
+    {:ok, input} = StringIO.open("""
+      12
+      14
+      1969
+      100756
+      """)
+
+    %{input: IO.stream(input, :line)}
+  end
+
+  test "total_fuel_requirement", %{input: input} do
+    assert Aoc2019.Day1.total_fuel_requirement(input) == 34241
+  end
+
+  test "part_one" do
+    file_path = "lib/day1/input.txt"
+
+    assert Aoc2019.Day1.part_one(file_path) == 3443395
+  end
 end
