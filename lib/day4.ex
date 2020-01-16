ea7ddd6ab23836e3c7024594788deb87616c0c16
Yurii Skrynnykov <truef1s7@gmail.com>
2020-01-17 00:26:00 +0200

    Add day 4 part 2 solution.

--- lib/day4.ex
+++ lib/day4.ex
@@ -4,12 +4,17 @@ defmodule Aoc2019.Day4 do
   @spec part_one(filename :: Path.t()) :: integer
   def part_one(filename \\ "lib/day4/input.txt") do
     filename
-    |> File.read!()
-    |> String.replace("-", "..")
-    |> Code.eval_string
-    |> elem(0)
-    |> Stream.map(&Integer.digits/1)
-    |> Stream.filter(&validate/1)
+    |> parse_input()
+    |> Stream.filter(&validate_part_one/1)
+    |> Enum.count()
+  end
+
+  @spec part_two(filename :: Path.t()) :: integer
+  def part_two(filename \\ "lib/day4/input.txt") do
+    filename
+    |> parse_input()
+    |> Stream.filter(&validate_part_one/1)
+    |> Stream.filter(&validate_part_two/1)
     |> Enum.count()
   end
 
@@ -17,8 +22,8 @@ defmodule Aoc2019.Day4 do
   Validates, if list of digits have same adjacent digits and non decreasing digits.
   Returns `true` or `false`.
   """
-  @spec validate(digits :: list) :: boolean
-  def validate(digits) do
+  @spec validate_part_one(digits :: list) :: boolean
+  def validate_part_one(digits) do
     {_helper_digit, result} =
       Enum.reduce_while(digits, {0, false}, fn digit, {previous, has_same_adjacent} ->
         cond do
@@ -27,6 +32,44 @@ defmodule Aoc2019.Day4 do
           true -> {:halt, {0, false}}
         end
       end)
+
     result
+  end
+
+  @doc """
+  Validates, if list of digits have at least one or more groups of only two same
+  adjacent digits. Returns `true` or `false`.
+  """
+  @spec validate_part_two(digits :: list) :: boolean
+  def validate_part_two(digits) do
+    digits
+    |> split_on_same_digits_groups()
+    |> Enum.any?(fn same_digits -> tuple_size(same_digits) == 2 end)
+  end
+
+  @spec split_on_same_digits_groups(digits :: list) :: list(tuple)
+  def split_on_same_digits_groups([digit | digits]) do
+    split_on_same_digits_groups(digits, {{digit}, []})
+  end
+
+  defp split_on_same_digits_groups([], {last_processed, rest}), do: [last_processed | rest]
+
+  defp split_on_same_digits_groups([digit | digits], {current_same_group, acc})
+       when elem(current_same_group, 0) == digit do
+    split_on_same_digits_groups(digits, {Tuple.append(current_same_group, digit), acc})
+  end
+
+  defp split_on_same_digits_groups([digit | digits], {current_same_group, acc}) do
+    split_on_same_digits_groups(digits, {{digit}, [current_same_group | acc]})
+  end
+
+  @spec parse_input(filename :: Path.t()) :: Enumerable.t()
+  defp parse_input(filename) do
+    filename
+    |> File.read!()
+    |> String.replace("-", "..")
+    |> Code.eval_string()
+    |> elem(0)
+    |> Stream.map(&Integer.digits/1)
   end
 end
--- test/day4_test.exs
+++ test/day4_test.exs
@@ -1,12 +1,21 @@
 defmodule Aoc2019.Day4Test do
   use ExUnit.Case, async: true
 
-  test "validate/1" do
-    numbers =
+  test "validate_part_one/1" do
+    results =
       [111111, 223450, 123789]
       |> Enum.map(fn number ->
-        Integer.digits(number) |> Aoc2019.Day4.validate()
+        Integer.digits(number) |> Aoc2019.Day4.validate_part_one()
       end)
-    assert numbers === [true, false, false]
+    assert results == [true, false, false]
+  end
+
+  test "validate_part_two/1" do
+    results =
+      [112233, 123444, 111122]
+      |> Enum.map(fn number ->
+        Integer.digits(number) |> Aoc2019.Day4.validate_part_two()
+      end)
+    assert results == [true, false, true]
   end
 end
